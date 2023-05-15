78684e6c0cf8a5b55f8eaf556bf0cdbc8650152e
Rangga857 <azharrangga28@gmail.com>
2023-05-15 15:54:37 +0700

    membuat printAll untuk menampilkan di layar

--- MbrStatikFunc/MbrStatikFunc.cpp
+++ MbrStatikFunc/MbrStatikFunc.cpp
@@ -32,7 +32,15 @@ void mahasiswa::printAll() {
 int main() {
 	mahasiswa mhs1("Sri Dadi");
 	mahasiswa mhs2("Budi Jatmiko");
-	mahasiswa::setnim(9); //mengakses nim melalui static member  function "setnim?
+	mahasiswa::setnim(9); //mengakses nim melalui static member  function "setnim"
 	mahasiswa mhs3("Andi Janu");
 	mahasiswa mhs4("Joko Wahoo");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "Akses dari luar object = " << mahasiswa::getNim() << endl; // mengaskes nim melalui static meber function "getNim"
+
 }	
