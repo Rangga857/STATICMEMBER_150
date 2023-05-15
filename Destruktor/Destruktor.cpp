fcefbe8b49005a57347d98bcf84db1b7200cc7db
Rangga857 <azharrangga28@gmail.com>
2023-05-15 15:28:13 +0700

    Desstructor dipanggil

--- Destruktor/Destruktor.cpp
+++ Destruktor/Destruktor.cpp
@@ -41,4 +41,7 @@ void angka::isiData() {
 int main() {
 	angka belajarcpp(3); //Constructor dipanggil
 	angka* ptrBelajarcpp = new angka(5); // constructor dipanggil
+	delete ptrBelajarcpp; // Destructor dipanggil
+
+	
 }
