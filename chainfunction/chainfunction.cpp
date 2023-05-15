2c69514265f9048713beaa61be652785022a592f
Rangga857 <azharrangga28@gmail.com>
2023-05-15 15:12:57 +0700

    membuat int main untuk menampilkan di layar

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -3,4 +3,23 @@ using namespace std;
 class buku
 {
 	string judul;
-};
+
+public : 
+	buku setJudul(string judul)
+	{
+		this->judul = judul;
+		return *this; // chain function
+	}
+	string getJudul()
+	{
+		return this->judul;
+	}
+}bukunya;
+
+int main()
+{
+	//bukunya.setJudul("Matematika")
+	// cout << bukunya.gettJudul();
+	cout << bukunya.setJudul("Matematika").getJudul();//chain functio calls
+	return 0;
+}
