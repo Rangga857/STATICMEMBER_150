1396335134b855c20404cc0e5dd7fffb7cfa8c84
Rangga857 <azharrangga28@gmail.com>
2023-05-15 15:35:37 +0700

    membuat definisi member function

--- Konstruktor/Konstruktor.cpp
+++ Konstruktor/Konstruktor.cpp
@@ -13,3 +13,27 @@ public :
 	mahasiswa(int iNim, string iNama);
 	void cetak();
 };
+
+mahasiswa::mahasiswa() {
+	nim = 0;
+	nama = " ";
+}
+
+mahasiswa::mahasiswa(int iNim) {
+	nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama) {
+	nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
+
+void mahasiswa::cetak() {
+	cout << "Nim = " << endl;
+	cout << "nama = " << nama << endl;
+}
+
